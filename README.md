7c872f69fcf2c9521065d5b0274f2293c16e9e31
Joe McManus <joe.mcmanus@colorado.edu>
2016-02-18 15:03:01 -0700

    Adding LED script

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ Intel Galileo LED python script
     positional arguments:
       pinNumber      Specify the pin number, i.e. 0-13
 
-   optional arguments:
-     -h, --help     show this help message and exit
-    --count COUNT  Number of times to execute, default infinity
-    --delay DELAY  Number of seconds to wait between readings, default 10
-    --version      show program's version number and exit
+    optional arguments:
+      -h, --help     show this help message and exit
+      --count COUNT  Number of times to execute, default infinity
+      --delay DELAY  Number of seconds to wait between readings, default 10
+      --version      show program's version number and exit
  
---
+++ led.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# File    : led.py 
+# Author  : Joe McManus josephmc@alumni.cmu.edu
+# Version : 0.1  02/17/2016
+# Copyright (C) 2016 Joe McManus
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import mraa
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='Simple LED Blinker for Galileo')
+parser.add_argument('pinNumber', help="Specify the pin number, i.e. 0-13", type=int)
+parser.add_argument('--count', help="Number of times to execute, default infinity", default=0, type=int, action="store")
+parser.add_argument('--delay', help="Number of seconds to wait between readings, default 10", default=10, type=int, action="store")
+parser.add_argument('--version', action='version',version='%(prog)s 0.1')
+args=parser.parse_args()
+
+
+#Initialize the MRAA pin
+pin = mraa.Gpio(int(args.pinNumber))
+pin.dir(mraa.DIR_OUT)
+pin.write(0)
+print("Blinking LED {:d}". format(args.pinNumber))
+print("Using delay {:d} and for {:d} attempts".format(args.delay, args.count))
+
+i = 0 
+while True: 
+	try:
+		print("Turning LED {:d} on, for {:d} seconds, count {:d}.".format(args.pinNumber, args.delay, i))
+		pin.write(1)
+		i = i+1
+		if args.count != 0:
+			if args.count == i:
+				break
+		time.sleep(args.delay)
+
+		print("Turning LED {:d} off, for {:d} seconds.".format(args.pinNumber, args.delay))
+		pin.write(0)
+		time.sleep(args.delay)
+	
+	except KeyboardInterrupt:
+		quit()
+
+	except Exception,e: 
+		print("Error: {:s}". format(e))
+		quit()
+	
